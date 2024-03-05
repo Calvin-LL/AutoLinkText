14728b625eafeb61a3b62a5d42f6bd92a7388c44
Calvin Liang <me@calvin.sh>
2024-03-04 23:55:11 -0800

    add examples to demo app

--- demoApp/src/main/java/sh/calvin/autolinktext/demo/MainActivity.kt
+++ demoApp/src/main/java/sh/calvin/autolinktext/demo/MainActivity.kt
@@ -3,13 +3,27 @@ package sh.calvin.autolinktext.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import sh.calvin.autolinktext.AutoLinkText
+import sh.calvin.autolinktext.MatchStyle
+import sh.calvin.autolinktext.TextRule
 import sh.calvin.autolinktext.demo.theme.AutoLinkTextTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +31,76 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AutoLinkTextTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            AutoLinkText(
+                text = "Visit https://www.google.com\n" +
+                        "Visit www.google.com\n" +
+                        "Email test@example.com\n" +
+                        "Call 6045557890\n" +
+                        "Call +1 (604) 555-7890\n" +
+                        "Call 604-555-7890\n",
+            )
+
+            AutoLinkText(
+                text = "Visit https://www.google.com\n" +
+                        "Visit www.google.com\n" +
+                        "Email test@example.com\n" +
+                        "Call 6045557890\n" +
+                        "Call +1 (604) 555-7890\n" +
+                        "Call 604-555-7890\n",
+                style = TextStyle.Default.copy(
+                    textAlign = TextAlign.Center,
+                )
+            )
+
+            val linkColor = MaterialTheme.colorScheme.primary
+
+            AutoLinkText(
+                text = "Visit https://www.google.com\n" +
+                        "Visit www.google.com\n" +
+                        "Email test@example.com\n" +
+                        "Call 6045557890\n" +
+                        "Call +1 (604) 555-7890\n" +
+                        "Call 604-555-7890\n",
+                textRules = TextRule.defaultList(LocalContext.current).map {
+                    it.copy(
+                        matchStyleProvider = {
+                            MatchStyle.SpanStyle(
+                                SpanStyle(
+                                    color = linkColor,
+                                    textDecoration = TextDecoration.Underline
+                                )
+                            )
+                        }
+                    )
+                }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AutoLinkTextTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
